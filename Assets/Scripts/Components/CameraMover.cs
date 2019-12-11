8e8abc548ee706ad1c9094a5999c538ed37b84dd
Dmitry <tda777@tut.by>
2019-12-11 21:29:42 +0300

    init

---
+++ Assets/Scripts/Components/CameraMover.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMover : MonoBehaviour
+{
+
+    void Update()
+    {
+        Vector3 forward = new Vector3(0.0f, 0.0f, 1.0f);
+
+        float speed = 3.0f;
+
+        Vector3 translation = forward * speed * Time.deltaTime;
+
+        transform.position += translation;
+    }
+}

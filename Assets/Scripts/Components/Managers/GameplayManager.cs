8cdb4457f7d9fd747d6ddca3ee7aab1af69559c4
Andrey <andrey.makarenok01@gmail.com>
2019-12-18 21:25:48 +0300

    Prefabs

---
+++ Assets/Scripts/Components/Managers/GameplayManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameplayManager : MonoBehaviour
+{
+    void Start()
+    {
+        var settingsManager = SettingsManager.Instance;
+        int index = Random.Range(0, settingsManager.ChunkList.Count);
+        var chunkPrefab = settingsManager.ChunkList[index];
+        var newChunk = Instantiate(chunkPrefab);
+    }
+
+}
---
+++ Assets/Scripts/Components/Managers/SettingsManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsManager : MonoBehaviour
+{
+    public static SettingsManager Instance = null;
+
+    [SerializeField]
+    public float CameraSpeed = 2.0f;
+    [SerializeField]
+    public List<GameObject> ChunkList = null; 
+
+    private void Start()
+    {
+        Instance = this;
+    }
+}

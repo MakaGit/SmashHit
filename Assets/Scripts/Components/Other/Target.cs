803181536dda9649dcc555b04794803d46904e75
Andrey <andrey.makarneok01@gmail.com>
2019-12-29 15:20:34 +0300

    TargetDestroy

---
+++ Assets/Scripts/Components/Other/Target.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _nonDestroyedDummy = null;
+    [SerializeField]
+    private GameObject _destroyedDummy = null;
+
+    private void Awake()
+    {
+        _nonDestroyedDummy.SetActive(true);
+        _destroyedDummy.SetActive(false);
+    }
+
+    private void OnVollisionEnter(Collision other)
+    {
+        _nonDestroyedDummy.SetActive(false);
+        _destroyedDummy.SetActive(true);
+    }
+}
